5e5fb06dfd1a967824e9e8fb2caa5633ceb05648
Cain Hall <cain.hall98@gmail.com>
2018-09-13 19:37:09 +1000

    redo room/player sockets

--- src/api/controllers/room.controller.js
+++ src/api/controllers/room.controller.js
@@ -59,39 +59,43 @@ exports.create = async (req, res, next) => {
  * Join a roo
  * @public
  */
-exports.join = async (req, res, next) => {
+exports.join = async (roomId, userId) => {
   try {
-    const selectedRoom = await Room.findOne({ _id: req.params.id }, (err, room) => {
+    const selectedRoom = await Room.findOne({ _id: roomId }, (err, room) => {
       if (err) {
-        return notFound(req, res, next);
+        console.log(err)
+        // return notFound(req, res, next);
       }
       return room;
     });
     const { players } = selectedRoom;
-    const { userId } = req.query;
+    const player = userId && players.find(x => `${x._id}` === userId);
+    // const { userId } = req.query;
 
     // if userId is sent, and is already a player
-    if (userId && players.find(x => `${x._id}` === userId)) {
-      return res.json({ room: selectedRoom, player: players.find(x => `${x._id}` === userId) });
+    if (player) {
+      // return res.json({ room: selectedRoom, player: players.find(x => `${x._id}` === userId) });
+      return { room: selectedRoom, player, status: 'joined' }
     }
 
     // room only allows 4 players
     if (players.length > 3) {
-      return roomFull(req, res, next);
+      return { room: selectedRoom, status: 'full' }
     }
 
     // add the new player
-    await selectedRoom.players.push({
-      team: blocks.teams[players.length],
-    });
+    // await selectedRoom.players.push({
+    //   team: blocks.teams[players.length],
+    // });
 
-    // push new player blocks into room
-    await selectedRoom.blocks.push(
-      ...blocks.createBlocks(selectedRoom.players[selectedRoom.players.length - 1].team),
-    );
-    await selectedRoom.save();
-    return res.json({ room: selectedRoom, player: players[players.length - 1] });
+    // // push new player blocks into room
+    // await selectedRoom.blocks.push(
+    //   ...blocks.createBlocks(selectedRoom.players[selectedRoom.players.length - 1].team),
+    // );
+    // await selectedRoom.save();
+    return { room: selectedRoom, player: players[players.length - 1] };
   } catch (e) {
+    console.log(e);
     return next(e);
   }
 };
--- src/api/sockets/block.js
+++ src/api/sockets/block.js
@@ -1,13 +1,14 @@
-const controller = require('../controllers/room.controller');
+const roomController = require('../controllers/room.controller');
 
 module.exports = function sockets(io) {
   io.on('connection', (socket) => {
+    const socketId = socket.id;
     socket.emit('connected');
 
     socket.on('block_select', () => {});
 
     socket.on('block_move', (data) => {
-      controller.moveBlock(data.roomId, data.block, data.userId).then((res) => {
+      roomController.moveBlock(data.roomId, data.block, data.userId).then((res) => {
         socket.broadcast.emit('blockMove', {
           block: res,
         });
@@ -16,10 +17,26 @@ module.exports = function sockets(io) {
 
     socket.on('block_edit', (data) => {
       console.log('block edit')
-      controller.editBlock(data.roomId, data.block, data.userId).then((res) => {
+      roomController.editBlock(data.roomId, data.block, data.userId).then((res) => {
         socket.broadcast.emit('blockEdit', {
           block: res,
         });
+      });
+    });
+    socket.on('room_connect', ({ roomId, userId }) => {
+      // status can be: joined, select, full
+      roomController.join(roomId, userId).then(({ room, player, status }) => {
+        io.to(`${socketId}`).emit('roomJoined', { room, player, status: 'select' });
+      });
+    });
+    socket.on('player_connect', ({ roomId, userId }) => {
+      roomController.playerSelect(roomId, userId).then(({ room, player }) => {
+        socket.broadcast.emit('playerJoined', {
+          room,
+          player,
+        });
+        io.to(`${socketId}`).emit('roomJoined', { room, player, status: 'joined' });
+        // io.to(`${socketId}`).emit('blockData', { blocks: room.blocks });
       });
     });
     socket.on('disconnect', () => {
